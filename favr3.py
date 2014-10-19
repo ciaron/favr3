4f16e9318c143704c6751ed72361568673bf6277
Ciaron Linstead <ciaron.linstead@gmail.com>
2014-10-19 19:43:15 +0200

    working version

--- favr3.py
+++ favr3.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask_bootstrap import Bootstrap
 from flickrAPI import FlickrAPI
 
@@ -12,12 +13,21 @@ def index():
 
 @app.route('/user/')
 @app.route('/user/<user_id>')
-def faves(user_id=None):
+def faves(user_id=None, page=1):
     """ show faves for user=<user_id>
     """
-    faves = flickr.favorites_getList(user_id=flickr.user_id, page=1, per_page=20)
+    try: 
+        page = request.args.get('page')
+    except:
+        pass
+
+    if user_id == None:
+        user_id = flickr.user_id
+
+    username = flickr.people_getInfo(user_id=user_id)['person']['username']['_content']
+    faves = flickr.favorites_getList(user_id=user_id, page=page, per_page=12, extras='owner_name')
     
-    return render_template('faves.html', user_id=user_id, faves=faves)
+    return render_template('faves.html', user_id=user_id, faves=faves, username=username)
 
 if __name__ == '__main__':
     Bootstrap(app)
